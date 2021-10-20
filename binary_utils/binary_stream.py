d936a55c98af5835cd3c4a5c2b955ca7bb8e0a09
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-10-20 13:37:34 +0300

    Update binary_stream.py

--- binary_utils/binary_stream.py
+++ binary_utils/binary_stream.py
@@ -29,9 +29,9 @@
 #                                                                              #
 ################################################################################
 
-from binary_utils.binary_converter import binary_converter
+from binary_utils.binary_converter import BinaryConverter
 
-class binary_stream:
+class BinaryStream:
     def __init__(self, data: bytes = b"", pos: int = 0) -> None:
         self.data: bytes = data
         self.pos: int = pos
@@ -50,142 +50,142 @@ class binary_stream:
         return bool(len(self.data) <= self.pos)
         
     def read_byte(self) -> int:
-        return binary_converter.read_byte(self.read(1))
+        return BinaryConverter.read_byte(self.read(1))
     
     def write_byte(self, value: int) -> None:
-        self.write(binary_converter.write_byte(value))
+        self.write(BinaryConverter.write_byte(value))
         
     def read_unsigned_byte(self) -> int:
-        return binary_converter.read_unsigned_byte(self.read(1))
+        return BinaryConverter.read_unsigned_byte(self.read(1))
     
     def write_unsigned_byte(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_byte(value))
+        self.write(BinaryConverter.write_unsigned_byte(value))
         
     def read_bool(self) -> bool:
-        return binary_converter.read_bool(self.read(1))
+        return BinaryConverter.read_bool(self.read(1))
     
     def write_bool(self, value: bool) -> None:
-        self.write(binary_converter.write_bool(value))
+        self.write(BinaryConverter.write_bool(value))
         
     def read_short_be(self) -> int:
-        return binary_converter.read_short_be(self.read(2))
+        return BinaryConverter.read_short_be(self.read(2))
     
     def write_short_be(self, value: int) -> None:
-        self.write(binary_converter.write_short_be(value))
+        self.write(BinaryConverter.write_short_be(value))
         
     def read_unsigned_short_be(self) -> int:
-        return binary_converter.read_unsigned_short_be(self.read(2))
+        return BinaryConverter.read_unsigned_short_be(self.read(2))
     
     def write_unsigned_short_be(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_short_be(value))
+        self.write(BinaryConverter.write_unsigned_short_be(value))
         
     def read_short_le(self) -> int:
-        return binary_converter.read_short_le(self.read(2))
+        return BinaryConverter.read_short_le(self.read(2))
     
     def write_short_le(self, value: int) -> None:
-        self.write(binary_converter.write_short_le(value))
+        self.write(BinaryConverter.write_short_le(value))
         
     def read_unsigned_short_le(self) -> int:
-        return binary_converter.read_unsigned_short_le(self.read(2))
+        return BinaryConverter.read_unsigned_short_le(self.read(2))
     
     def write_unsigned_short_le(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_short_le(value))
+        self.write(BinaryConverter.write_unsigned_short_le(value))
 
     def read_triad_be(self) -> int:
-        return binary_converter.read_triad_be(self.read(3))
+        return BinaryConverter.read_triad_be(self.read(3))
     
     def write_triad_be(self, value: int) -> None:
-        self.write(binary_converter.write_triad_be(value))
+        self.write(BinaryConverter.write_triad_be(value))
         
     def read_unsigned_triad_be(self) -> int:
-        return binary_converter.read_unsigned_triad_be(self.read(3))
+        return BinaryConverter.read_unsigned_triad_be(self.read(3))
     
     def write_unsigned_triad_be(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_triad_be(value))
+        self.write(BinaryConverter.write_unsigned_triad_be(value))
         
     def read_triad_le(self) -> int:
-        return binary_converter.read_triad_le(self.read(3))
+        return BinaryConverter.read_triad_le(self.read(3))
     
     def write_triad_le(self, value: int) -> None:
-        self.write(binary_converter.write_triad_le(value))
+        self.write(BinaryConverter.write_triad_le(value))
         
     def read_unsigned_triad_le(self) -> int:
-        return binary_converter.read_unsigned_triad_le(self.read(3))
+        return BinaryConverter.read_unsigned_triad_le(self.read(3))
     
     def write_unsigned_triad_le(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_triad_le(value))
+        self.write(BinaryConverter.write_unsigned_triad_le(value))
         
     def read_int_be(self) -> int:
-        return binary_converter.read_int_be(self.read(4))
+        return BinaryConverter.read_int_be(self.read(4))
     
     def write_int_be(self, value: int) -> None:
-        self.write(binary_converter.write_int_be(value))
+        self.write(BinaryConverter.write_int_be(value))
         
     def read_unsigned_int_be(self) -> int:
-        return binary_converter.read_unsigned_int_be(self.read(4))
+        return BinaryConverter.read_unsigned_int_be(self.read(4))
     
     def write_unsigned_int_be(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_int_be(value))
+        self.write(BinaryConverter.write_unsigned_int_be(value))
         
     def read_int_le(self) -> int:
-        return binary_converter.read_int_le(self.read(4))
+        return BinaryConverter.read_int_le(self.read(4))
     
     def write_int_le(self, value: int) -> None:
-        self.write(binary_converter.write_int_le(value))
+        self.write(BinaryConverter.write_int_le(value))
         
     def read_unsigned_int_le(self) -> int:
-        return binary_converter.read_unsigned_int_le(self.read(4))
+        return BinaryConverter.read_unsigned_int_le(self.read(4))
     
     def write_unsigned_int_le(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_int_le(value))
+        self.write(BinaryConverter.write_unsigned_int_le(value))
 
     def read_long_be(self) -> int:
-        return binary_converter.read_long_be(self.read(8))
+        return BinaryConverter.read_long_be(self.read(8))
     
     def write_long_be(self, value: int) -> None:
-        self.write(binary_converter.write_long_be(value))
+        self.write(BinaryConverter.write_long_be(value))
         
     def read_unsigned_long_be(self) -> int:
-        return binary_converter.read_unsigned_long_be(self.read(8))
+        return BinaryConverter.read_unsigned_long_be(self.read(8))
     
     def write_unsigned_long_be(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_long_be(value))
+        self.write(BinaryConverter.write_unsigned_long_be(value))
         
     def read_long_le(self) -> int:
-        return binary_converter.read_long_le(self.read(8))
+        return BinaryConverter.read_long_le(self.read(8))
     
     def write_long_le(self, value: int) -> None:
-        self.write(binary_converter.write_long_le(value))
+        self.write(BinaryConverter.write_long_le(value))
         
     def read_unsigned_long_le(self) -> int:
-        return binary_converter.read_unsigned_long_le(self.read(8))
+        return BinaryConverter.read_unsigned_long_le(self.read(8))
     
     def write_unsigned_long_le(self, value: int) -> None:
-        self.write(binary_converter.write_unsigned_long_le(value))
+        self.write(BinaryConverter.write_unsigned_long_le(value))
         
     def read_float_be(self) -> int:
-        return binary_converter.read_float_be(self.read(4))
+        return BinaryConverter.read_float_be(self.read(4))
     
     def write_float_be(self, value: int) -> None:
-        self.write(binary_converter.write_float_be(value))
+        self.write(BinaryConverter.write_float_be(value))
         
     def read_float_le(self) -> int:
-        return binary_converter.read_float_le(self.read(4))
+        return BinaryConverter.read_float_le(self.read(4))
     
     def write_float_le(self, value: int) -> None:
-        self.write(binary_converter.write_float_le(value))
+        self.write(BinaryConverter.write_float_le(value))
         
     def read_double_be(self) -> int:
-        return binary_converter.read_double_be(self.read(8))
+        return BinaryConverter.read_double_be(self.read(8))
     
     def write_double_be(self, value: int) -> None:
-        self.write(binary_converter.write_double_be(value))
+        self.write(BinaryConverter.write_double_be(value))
         
     def read_double_le(self) -> int:
-        return binary_converter.read_double_le(self.read(8))
+        return BinaryConverter.read_double_le(self.read(8))
     
     def write_double_le(self, value: int) -> None:
-        self.write(binary_converter.write_double_le(value))
+        self.write(BinaryConverter.write_double_le(value))
         
     def read_var_int(self) -> int:
         value: int = 0
